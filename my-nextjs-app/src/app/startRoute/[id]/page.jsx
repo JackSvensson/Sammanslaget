7848ae007cae13d485538447e5f7493f18e3e665
TobiasAndren <tobiasandren@hotmail.com>
2025-08-21 10:28:43 +0200

    Added a timer and statboard to active route page

--- my-nextjs-app/src/app/startRoute/[id]/page.jsx
+++ my-nextjs-app/src/app/startRoute/[id]/page.jsx
@@ -4,13 +4,31 @@ import { routes } from "@/data/routes";
 import dynamic from "next/dynamic";
 import styles from "./startRoute.module.css";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function StartRoutePage({ params }) {
   const { id } = React.use(params);
   const route = routes.find((r) => r.id.toString() === id);
+
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (secs) => {
+    const minutes = Math.floor(secs / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (secs % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  };
 
   if (!route) return <p>Rutt ej hittad</p>;
 
@@ -59,6 +77,12 @@ export default function StartRoutePage({ params }) {
         </ul>
       </div>
       <button className={styles.stationButton}>Framme vid Station</button>
+      <div className={styles.statContainer}>
+        <p className={styles.statText}>
+          <small>Tid:</small>
+          {formatTime(seconds)}
+        </p>
+      </div>
     </main>
   );
 }
